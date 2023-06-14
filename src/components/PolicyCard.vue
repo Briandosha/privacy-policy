a65bd512c3d97016be290c1aa4f4d3caf121b036
unknown <briandosha@gmail.com>
2023-06-14 19:27:57 +0300

    initial commit

---
+++ src/components/PolicyCard.vue
@@ -0,0 +1,171 @@
+<template>
+    <div class="relative">
+      <div class="h-96 privacy-policy-header-bg-class bg-cover bg-center bg-no-repeat">
+      </div>
+      <div class="absolute inset-0 flex items-center justify-center h-screen ">
+        <div class=" max-w-4xl mx-auto bg-white rounded-sm shadow-md h-full overflow-y-scroll">
+          <div class="h-64 w-full">
+            <img
+              class="h-full w-full object-contain pt-20"
+              src="../assets/images/programmer_cartoon.jpg"
+              alt="Closeup code"
+            />
+          </div>
+          <div class=" p-8">
+            <div class="uppercase tracking-wide text-sm font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 text-center">
+              Privacy Policy
+            </div>
+            <a
+              href="#"
+              class="mt-1 text-lg tracking-tight font-medium font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 hover:underline">
+              Introduction
+            </a>
+            <p class="mt-2 text-sm text-slate-500">
+              Your right to privacy and security is very important to us. Doshacorp treats personal information obtained through the use of this website, electronic device and/or mobile solution/application or agent as private and confidential and are committed to providing you with the utmost secure access.
+            </p>
+
+            <div class="mt-1 text-lg tracking-tight font-medium font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 hover:underline">
+              Personal information
+            </div>
+
+            <p class="mt-2 text-sm text-slate-500">
+              Personal Information is information relating to an individual/ company/ entity/ group and comprises all the details Doshacorp holds or collects about you, directly or indirectly, your transactions, financial information, interactions and dealings with Doshacorp, including information received from third parties and information collected through use of our website and our electronic banking services.         
+        
+            </p>
+
+            <div class="indent-8 mt-1 text-lg tracking-tight font-medium font-extrabold text-transparent text-1xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 list-decimal">
+              A.)Purposes for use of your personal Information
+            </div> 
+            <p class="mt-2 text-sm text-slate-500">
+              Doshacorp is determined to meet all your needs and therefore may use and collect personal and/or information to meet our obligations. We have highlighted below some of the many uses of the collected information:        
+            </p>
+           
+              <ul class="pl-12 list-disc mt-2 text-sm text-slate-500">
+                <li>Statistical analysis</li>
+                <li>Operational purposes</li>
+                <li>Conducting market research and surveys</li>
+                <li>Assessing aptness for products and services</li>
+                <li>Management of banking relationships</li>
+                <li>Marketing, promotions, competitions</li>
+                <li>Compliance with both local or foreign law, agreements between any user of the Doshacorp products and any authority, regulator, or enforcement agency policies (including Doshacorp internal policies), good practice, government sanctions, reporting requirements under legislation, and demands or requests of any authority, regulator, tribunal, enforcement agency, or exchange body</li>
+                <li>Seeking professional advice, including, in connection with any and or prospective legal proceedings, for obtaining legal advice, or for establishing, exercising or defending Legal rights</li>
+                <li>Enable provision of products and services</li>
+                <li>Credit assessment</li>
+                <li>Prevention, detection, investigation and prosecution of crime in any jurisdiction, identity verification, government sanctioned screening and due diligence checks.</li>
+            </ul>
+           
+            <p class="mt-2 text-sm text-slate-500">
+              You acknowledge that the above information is by no means exhaustive and shall include any other reason Doshacorp in its discretion may reasonably deem necessary.
+            </p>
+
+            <p class="mt-2 text-sm text-slate-500">
+              We may disclose information to any member of Doshacorp, including our officers, employees, agents and advisers, and any of the following parties for any of the purposes stated above. The persons eligible to receive the information are:            
+            </p>
+           
+            <ul class="pl-12 list-disc mt-2 text-sm text-slate-500">
+                <li>Any member of Doshacorp anywhere in the world</li>
+                <li>Any person to whom disclosure is allowed or required by local or foreign law, regulation or any other applicable instrument</li>
+                <li>Professional advisers (including auditors), third party service providers, agents or independent contractors providing services to support Doshacorp's business</li>
+                <li>Any court, tribunal, regulator, enforcement agency, exchange body, tax authority, or any other authority or their agents</li>
+                <li>Any debt collection agency, credit bureau, insurer or insurance broker, direct or indirect provider of credit protection and fraud prevention agencies.</li>
+               
+            </ul>
+
+            <div class="indent-8 mt-1 text-lg tracking-tight font-medium font-extrabold text-transparent text-1xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 list-decimal">
+              B.)Disclosure
+            </div> 
+            <p class="mt-2 text-sm text-slate-500">
+              Doshacorp will not disclose personal information to anyone outside Doshacorp without your prior consent unless:
+            </p>
+
+            <ul class="pl-12 list-disc mt-2 text-sm text-slate-500">
+                <li>compelled by law</li>
+                <li>it is in the public interest to do so</li>
+                <li>it is necessary to protect our rights</li>
+              </ul>
+
+              <div class="indent-8 mt-1 text-lg tracking-tight font-medium font-extrabold text-transparent text-1xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 list-decimal">
+              C.)Security and liability
+            </div> 
+            <p class="mt-2 text-sm text-slate-500">
+              Doshacorp has set up technical and organizational security measures to safeguard your personal information. When using external service providers, we require that they adhere to the same security standards as Doshacorp.           
+            </p>
+            <p class="mt-2 text-sm text-slate-500">
+              Personal Information may be transferred to, or stored at, a location outside of your country of residence but regardless of the location, we shall take all steps reasonably necessary to ensure that personal information is safe and secure.
+            </p>
+
+            <p class="mt-2 text-sm text-slate-500">
+              You acknowledge herewith that the Internet is an insecure form of communication and carries with it risks including the risk of access and interference by unauthorized third parties. We do not therefore accept responsibility or liability for the confidentiality, security or integrity of your Personal Information in connection with its transmission over the Internet.
+            </p>
+
+            <div class="indent-8 mt-1 text-lg tracking-tight font-medium font-extrabold text-transparent text-1xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 list-decimal">
+              D.) Other Terms and Conditions
+            </div> 
+            <p class="mt-2 text-sm text-slate-500">
+              Due to some specific terms and conditions in some of our product agreements that govern the collection, use and disclosure of your Personal Information, such terms shall be construed in conjunction with this Privacy Policy.
+            </p>
+
+            <div class="indent-8 mt-1 text-lg tracking-tight font-medium font-extrabold text-transparent text-1xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 list-decimal">
+              E.) Retention
+            </div> 
+            <p class="mt-2 text-sm text-slate-500">
+              We retain your Personal Information for legal, regulatory, business and operational purposes.
+            </p>
+
+            <div class="indent-8 mt-1 text-lg tracking-tight font-medium font-extrabold text-transparent text-1xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 list-decimal">
+              F.)  Monitoring
+            </div> 
+            <p class="mt-2 text-sm text-slate-500">
+              With due regard to any Laws, we may record and monitor your electronic communications with us to ensure compliance with our Legal and regulatory obligations and internal policies for the purposes outlined above.
+            </p>
+
+            <div class="indent-8 mt-1 text-lg tracking-tight font-medium font-extrabold text-transparent text-1xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 list-decimal">
+              G.) Right of Access
+            </div> 
+            <p class="mt-2 text-sm text-slate-500">
+              Subject to applicable Law, regulations and/or industry guidelines, you have the right to access the Personal Information processed in relation to you expeditiously and at a reasonable cost.
+            </p>
+
+            <div class="indent-8 mt-1 text-lg tracking-tight font-medium font-extrabold text-transparent text-1xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 list-decimal">
+              G.) Changes to this policy
+            </div> 
+            <p class="mt-2 text-sm text-slate-500">
+              This Privacy Policy may be updated from time to time and shall be reviewed regularly. We reserve the right to amend this privacy and security statement at any time. You are advised to consult this Privacy Policy regularly for any changes.
+            </p>
+
+            <p class="mt-2 text-sm text-slate-500">
+              All amendments or updated final versions will be posted on the website. Unless otherwise stated, the current version shall supersede and replace all previous versions of this privacy and security statement policy.
+            </p>
+
+
+            <div class="indent-8 mt-1 text-lg tracking-tight font-medium font-extrabold text-transparent text-1xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 list-decimal">
+              I.) Cookies
+            </div> 
+            <p class="mt-2 text-sm text-slate-500">
+              On some parts of our website we use so-called "cookie" technology. Cookie technology consists of small pieces of information or a small text file which is given to your browser by our webserver when you visit our website. This information is stored on your browser. The cookie is sent back to our webserver each time you visit our website. Cookies are not computer programs and do not run on a computer like programs do. They cannot gather information or function on their own. They cannot collect any personal information about you or your machine and cannot gather information or information about what you do on the Internet. Cookies merely enable us to provide a more valuable online experience to you.
+            </p>
+
+            <p class="mt-2 text-sm text-slate-500">
+              While you can set up your Internet browser to disable cookie technology, we do not recommend you do this since some parts of this website and our online services may not function properly, or at all.
+            </p>
+
+            <div class="indent-8 mt-1 text-lg tracking-tight font-medium font-extrabold text-transparent text-1xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 list-decimal">
+              J.) Linked Websites
+            </div> 
+            <p class="mt-2 text-sm text-slate-500">
+              Our Policy does not apply to third-party websites where our online advertisements are displayed or to linked third-party websites not under our control.
+            </p>
+
+            <div class="indent-8 mt-1 text-lg tracking-tight font-medium font-extrabold text-transparent text-1xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 list-decimal">
+              K.) Communications
+            </div> 
+            <p class="mt-2 text-sm text-slate-500">
+              We may use your personal or other information to send you communication on new services or products that may be of interest to you and from time to time, mail, email or SMS information to you about us, our products and any other relevant information.
+            </p>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
